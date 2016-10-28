c77b6fc0ac3572ae27045991268ffafb9aee9286
Michael Schumacher <mschu528@gmail.com>
2016-10-27 21:43:20 -0500

    created sample text and arithmetic output programs

---
+++ chapter01/BasicArithmetic.java
@@ -0,0 +1,18 @@
+/*
+ * BasicArithmetic.java
+ * An initial program to demonstrate use of the Java language for basic
+ * mathematical computations.
+ *
+ * Author:   Michael K Schumacher
+ * Created:  27-10-2016
+ * Modified: 27-10-2016
+ */
+
+public class BasicArithmetic
+{
+    public static void main(String[] args)
+    {
+        // Will compute the expression and print the result to the console
+        System.out.println((27 + 13.5 * 3) / (12 + 4));
+    }
+}
---
+++ chapter01/Welcome.java
@@ -0,0 +1,18 @@
+/*
+ * Welcome.java
+ * An initial program to demonstrate the basics of the Java programming
+ * language by writing "Hello, World! Welcome to Java" to the console
+ *
+ * Author:   Michael K Schumacher
+ * Created:  27-10-2016
+ * Modified: 27-10-2016
+ */
+
+public class Welcome
+{
+    public static void main(String[] args)
+    {
+        // Will print the message to the console
+        System.out.println("Hello, World! Welcome to Java");
+    }
+}
