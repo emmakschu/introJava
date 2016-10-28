bb729bd5709445de3f07b57bb4137591ae9ef718
Michael Schumacher <mschu528@gmail.com>
2016-10-28 07:22:00 -0500

    added shell script to demonstrate creating, compiling, and running Java program from console

--- chapter01/firstJava.sh
+++ chapter01/firstJava.sh
@@ -8,8 +8,10 @@
 # Created:  27-10-2016
 # Modified: 27-10-2016
 
+# Create the file, if it doesn't already exist
 touch FirstJava.java
 
+# Write the code to a variable for simplicity
 code="public class FirstJava
 {
     public static void main(String[] args)
@@ -18,8 +20,11 @@ code="public class FirstJava
     }
 }"
 
+# Push the code to overwrite FirstJava.java
 echo $code > FirstJava.java
 
+# Compile the program with the javac command
 javac FirstJava.java
 
+# Run the program with the java command
 java FirstJava
